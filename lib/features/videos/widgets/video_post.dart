ba33e14a17a4326e1686f9d7dc1f57892ed81abb
gyute <9tempo@gmail.com>
2023-10-11 03:22:43 +0900

    Add basic video player functionality

---
+++ lib/features/videos/widgets/video_post.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPost extends StatefulWidget {
+  final Function onVideoFinished;
+
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+  });
+
+  @override
+  State<VideoPost> createState() => _VideoPostState();
+}
+
+class _VideoPostState extends State<VideoPost> {
+  final VideoPlayerController _videoPlayerController =
+      VideoPlayerController.asset("assets/videos/video.mp4");
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: _videoPlayerController.value.isInitialized
+              ? VideoPlayer(_videoPlayerController)
+              : Container(
+                  color: Colors.black,
+                ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initVideoPlayer();
+  }
+
+  void _initVideoPlayer() async {
+    await _videoPlayerController.initialize();
+    _videoPlayerController.play();
+    setState(() {});
+    _videoPlayerController.addListener(_onVideoChage);
+  }
+
+  void _onVideoChage() {
+    if (_videoPlayerController.value.isInitialized) {
+      if (_videoPlayerController.value.duration ==
+          _videoPlayerController.value.position) {
+        widget.onVideoFinished();
+      }
+    }
+  }
+}
