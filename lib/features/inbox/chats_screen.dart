b9c77e0bb012d5f322ee02a71e387e9fc2f540fa
gyute <9tempo@gmail.com>
2023-10-17 01:15:11 +0900

    Add the basic UI for details of a direct message(`DM`)

---
+++ lib/features/inbox/chats_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/inbox/chat_detail_screen.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  final Duration _duration = const Duration(milliseconds: 300);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Direct messages"),
+        elevation: 1,
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const FaIcon(
+              FontAwesomeIcons.plus,
+            ),
+          )
+        ],
+      ),
+      body: AnimatedList(
+        key: _key,
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: _makeTile(index),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: _duration,
+      );
+      _items.add(_items.length);
+    }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: _makeTile(index),
+        ),
+        duration: _duration,
+      );
+      _items.remove(index);
+    }
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onTap: _onChatTap,
+      onLongPress: () => _deleteItem(index),
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage: NetworkImage(
+            "https://avatars.githubusercontent.com/u/15077948?v=4"),
+        child: Text("GT"),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "M ($index)",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          )
+        ],
+      ),
+      subtitle: const Text("Don't forget to message me"),
+    );
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ChatDetailScreen(),
+      ),
+    );
+  }
+}
