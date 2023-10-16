210f21c8ff0a2b939be8e9290f760459fcee5d66
gyute <9tempo@gmail.com>
2023-10-16 18:11:33 +0900

    Modified the Row so that the title is centered

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -73,6 +73,7 @@ class _ActivityScreenState extends State<ActivityScreen>
           onTap: _toggleAnimations,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
             children: [
               const Text("All activity"),
               Gaps.h2,
