a399374bc6e1b432af9b087186a898dc9daf1f32
gyute <9tempo@gmail.com>
2023-10-18 23:29:16 +0900

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
