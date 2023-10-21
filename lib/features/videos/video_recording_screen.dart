c3095719912bee4dceabff567b7f6bf888322768
gyute <9tempo@gmail.com>
2023-10-21 21:56:56 +0900

    Implement a video record button

--- lib/features/videos/video_recording_screen.dart
+++ lib/features/videos/video_recording_screen.dart
@@ -11,10 +11,25 @@ class VideoRecordingScreen extends StatefulWidget {
   State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
 }
 
-class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
+class _VideoRecordingScreenState extends State<VideoRecordingScreen>
+    with TickerProviderStateMixin {
   late CameraController _cameraController;
   bool _hasPermission = false;
   bool _isSelfieMode = false;
+  late final AnimationController _buttonAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 200),
+  );
+  late final Animation<double> _buttonAnimation =
+      Tween(begin: 1.0, end: 1.3).animate(_buttonAnimationController);
+  late final AnimationController _progressAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 10),
+    lowerBound: 0.0,
+    upperBound: 1.0,
+  );
   late FlashMode _flashMode;
 
   @override
@@ -87,6 +102,38 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
                         ),
                       ],
                     ),
+                  ),
+                  Positioned(
+                    bottom: Sizes.size40,
+                    child: GestureDetector(
+                      onTapDown: _startRecording,
+                      onTapUp: (details) => _stopRecording(),
+                      child: ScaleTransition(
+                        scale: _buttonAnimation,
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            SizedBox(
+                              width: Sizes.size96,
+                              height: Sizes.size96,
+                              child: CircularProgressIndicator(
+                                color: Theme.of(context).primaryColor,
+                                strokeWidth: Sizes.size6,
+                                value: _progressAnimationController.value,
+                              ),
+                            ),
+                            Container(
+                              width: Sizes.size80,
+                              height: Sizes.size80,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -114,7 +161,6 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   Future<void> initPermissions() async {
     final cameraPermission = await Permission.camera.request();
     final micPermission = await Permission.microphone.request();
-
     final cameraDenied =
         cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
     final micDenied =
@@ -134,6 +180,18 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
   void initState() {
     super.initState();
     initPermissions();
+    _progressAnimationController.addListener(
+      () {
+        setState(() {});
+      },
+    );
+    _progressAnimationController.addStatusListener(
+      (status) {
+        if (status == AnimationStatus.completed) {
+          _stopRecording();
+        }
+      },
+    );
   }
 
   Future<void> _setFlashMode(FlashMode newFlashMode) async {
@@ -142,9 +200,18 @@ class _VideoRecordingScreenState extends State<VideoRecordingScreen> {
     setState(() {});
   }
 
+  void _startRecording(TapDownDetails _) {
+    _buttonAnimationController.forward();
+    _progressAnimationController.forward();
+  }
+
+  void _stopRecording() {
+    _buttonAnimationController.reverse();
+    _progressAnimationController.reset();
+  }
+
   Future<void> _toggleSelfieMode() async {
     _isSelfieMode = !_isSelfieMode;
-
     await initCamera();
     setState(() {});
   }
