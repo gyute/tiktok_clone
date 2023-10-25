86a54a7e59d7d2ad128fd65973007874960e8281
gyute <9tempo@gmail.com>
2023-10-25 21:10:03 +0900

    Added user login authentication

--- lib/main.dart
+++ lib/main.dart
@@ -45,13 +45,13 @@ void main() async {
   );
 }
 
-class TikTokApp extends StatelessWidget {
+class TikTokApp extends ConsumerWidget {
   const TikTokApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
-      routerConfig: router,
+      routerConfig: ref.watch(routerProvider),
       // TODO: FOR DEV
       // locale: Locale("ko"),
       debugShowCheckedModeBanner: false,
