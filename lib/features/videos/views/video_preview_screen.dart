260d0cd8f3afcd88c0ac6d58144234f2a949c85f
gyute <9tempo@gmail.com>
2023-10-25 16:48:37 +0900

    Added skeleton for video upload

--- lib/features/videos/views/video_preview_screen.dart
+++ lib/features/videos/views/video_preview_screen.dart
@@ -2,11 +2,13 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gallery_saver/gallery_saver.dart';
+import 'package:tiktok_clone/features/videos/view_models/timeline_view_model.dart';
 import 'package:video_player/video_player.dart';
 
-class VideoPreviewScreen extends StatefulWidget {
+class VideoPreviewScreen extends ConsumerStatefulWidget {
   final XFile video;
   final bool isPicked;
 
@@ -17,10 +19,10 @@ class VideoPreviewScreen extends StatefulWidget {
   });
 
   @override
-  State<VideoPreviewScreen> createState() => _VideoPreviewScreenState();
+  VideoPreviewScreenState createState() => VideoPreviewScreenState();
 }
 
-class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
+class VideoPreviewScreenState extends ConsumerState<VideoPreviewScreen> {
   late final VideoPlayerController _videoPlayerController;
   bool _saveVideo = false;
 
@@ -34,10 +36,18 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
           if (!widget.isPicked)
             IconButton(
               onPressed: _saveToGallery,
-              icon: FaIcon(_saveVideo
-                  ? FontAwesomeIcons.check
-                  : FontAwesomeIcons.download),
+              icon: FaIcon(
+                _saveVideo ? FontAwesomeIcons.check : FontAwesomeIcons.download,
+              ),
             ),
+          IconButton(
+            onPressed: ref.watch(timelineProvider).isLoading
+                ? () {}
+                : _onUploadPressed,
+            icon: ref.watch(timelineProvider).isLoading
+                ? const CircularProgressIndicator.adaptive()
+                : const FaIcon(FontAwesomeIcons.cloudArrowUp),
+          )
         ],
       ),
       body: _videoPlayerController.value.isInitialized
@@ -71,6 +81,10 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
     await _videoPlayerController.play();
 
     setState(() {});
+  }
+
+  void _onUploadPressed() {
+    ref.read(timelineProvider.notifier).uploadVideo();
   }
 
   Future<void> _saveToGallery() async {
